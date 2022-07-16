99f9f2248c7aa30c4952fb935e24e0cd82606ce8
estebanchiaro <estebanchiaromonte95@gmail.com>
2022-07-16 13:47:17 -0400

    update

--- projects.js
+++ projects.js
@@ -18,6 +18,26 @@ galleryImages.forEach(function(image, index){
     newImg.classList.remove('services-cell_img')
     newImg.classList.add('popup-img');
     newImg.setAttribute('id', 'current-img');
+
+    newImg.onload= function(){
+      let newNextBtn = document.createElement('a');
+      newNextBtn.innerHTML= '<i class="fas fa-chevron-right next"></i>';
+      container.appendChild(newNextBtn);
+      newNextBtn.setAttribute('class', 'img-btn-next');
+      newNextBtn.setAttribute('onclick', 'changeImg(1)');
+
+      let newPrevBtn = document.createElement('a');
+      newPrevBtn.innerHTML= '<i class="fas fa-chevron-left next"></i>';
+      container.appendChild(newPrevBtn);
+      newPrevBtn.setAttribute('class', 'img-btn-prev');
+      newPrevBtn.setAttribute('onclick', 'changeImg(0)');
+    }
   }
 
 })
+
+function closeImg(){
+  document.querySelector('.img-window').remove();
+  document.querySelector('.img-btn-next').remove();
+  document.querySelector('.img-btn-prev').remove();
+}
